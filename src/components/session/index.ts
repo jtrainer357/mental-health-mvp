0c7a57ffcef60afb89ba5d1f4014256c581adec0
jtrainer357 <142727073+jtrainer357@users.noreply.github.com>
2026-02-08 01:31:47 -0500

    untracked files on feat/beta-patient-production: b05bbb9 feat(patients): add new patient modal with duplicate detection

---
+++ src/components/session/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Session Components
+ * @module components/session
+ */
+
+export { SessionTimer } from "./SessionTimer";
+export { SignAndLockDialog } from "./SignAndLockDialog";
+export { AddendumSection } from "./AddendumSection";
+export { AutoSaveIndicator } from "./AutoSaveIndicator";
